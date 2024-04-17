e9e732b67f0c26b5d52623377fd863b8e3c282c5
Александр Сагдинов <sagdinov.a@yandex.ru>
2024-04-17 12:40:35 +0300

    init server

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+const PORT = '8443';
+
+const doLog = console.log;
+
+var key = fs.readFileSync(__dirname + '/certs/device.key');
+var cert = fs.readFileSync(__dirname + '/certs/192.168.1.106.crt');
+
+// doLog(key, cert);
+
+const options = {
+    key,
+    cert
+};
+
+
+const app = express();
+
+
+
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const server = https.createServer(options, app);
+server.listen(PORT, () => {
+    doLog("server starting on port : " + PORT)
+  });
